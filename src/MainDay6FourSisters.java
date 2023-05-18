024639de1aaa240674719fd61f8a6070110555d9
tltl111 <tlafeber@outlook.com>
2023-05-18 22:01:38 +0200

    Added Day 7 egg calculator.

---
+++ src/MainDay6FourSisters.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MainDay6FourSisters {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many eggs have been gathered today?\nAmount: ");
+        int eggAmount = input.nextInt();
+
+        ChocEggCalculator splitEggs = new ChocEggCalculator(eggAmount);
+        int eggsForDuckbear;
+        int eggsForEachSister;
+        eggsForDuckbear = splitEggs.splittingEggsForDuckbear();
+        eggsForEachSister = splitEggs.splittingEggsForEachSister();
+
+        System.out.println("Each sister gets " + eggsForEachSister);
+        System.out.println("The duckbear gets " + eggsForDuckbear + " eggs.");
+
+        input.close();
+    }
+}
