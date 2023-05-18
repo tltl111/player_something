22339c10176fddec94a1fdcc0acfc4e68a4c45a9
tltl111 <tlafeber@outlook.com>
2023-05-18 20:43:35 +0200

    Added a triangle area calulator.

---
+++ src/CalculateTriangleArea.java
@@ -0,0 +1,27 @@
+public class CalculateTriangleArea {
+    private double base;
+    private double height;
+
+    public CalculateTriangleArea(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double calculateArea() {
+        return (base * height) / 2;
+    }
+}
