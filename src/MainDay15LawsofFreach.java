5fc04cb94c3ab23ee10f7af83aae8f34f82fad39
tltl111 <tlafeber@outlook.com>
2023-05-21 14:03:06 +0200

    Added MainDay15LawsofFreach

---
+++ src/MainDay15LawsofFreach.java
@@ -0,0 +1,24 @@
+public class MainDay15LawsofFreach {
+    public static void main(String[] args) {
+        
+        int[] array = { 4, 51, -7, 13, -99, 15, -8, 45, 90};
+        int currentSmallest = Integer.MAX_VALUE;
+
+        for (int index : array) {
+            if (index < currentSmallest) {
+            currentSmallest = index;
+            }
+        }
+
+        System.out.println(currentSmallest);
+
+        int total = 0;
+
+        for (int index : array)
+            total += index;
+            double average = (double)total / array.length;     
+        
+        System.out.println(average);
+
+    }
+}
