a9656a1f475dd2a472b1a971ad5c1930dde82cb1
tltl111 <tlafeber@outlook.com>
2023-05-21 16:15:32 +0200

    Day 17 recursionTest

---
+++ src/recursionTest.java
@@ -0,0 +1,15 @@
+public class recursionTest {
+    public static void main(String[] args) {
+
+        printNumbers(10);
+
+    }
+
+    public static void printNumbers(int number) {
+        if (number > 0) {
+            System.out.println(number);
+            printNumbers(number - 1);
+        }
+    }
+    
+}
