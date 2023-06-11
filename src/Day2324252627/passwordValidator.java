d3ef309ec4d894b4f04a4b54a8d59ae429936c78
tltl111 <tlafeber@outlook.com>
2023-06-11 19:11:04 +0200

    Started passwordValidator

---
+++ src/Day2324252627/passwordValidator.java
@@ -0,0 +1,10 @@
+package Day2324252627;
+
+public class passwordValidator {
+    
+
+}
+
+for (char ch: word.toCharArray()) { if (Character.isUpperCase(ch)) } Character has
+        static methods to categorize letters! char.isUpperCase('A'),
+        char.isLowerCase('a'), char.isDigit('0')
